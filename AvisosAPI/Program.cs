fd33a652fef8fa6912f50c6bcdee5b4c78a42975
Carlossl29 <221G0296@rcarbonifera.tecnm.mx>
2026-04-18 19:29:39 -0600

    quitar antiguo proyecto

--- AvisosAPI/Program.cs
+++ AvisosAPI/Program.cs
@@ -4,15 +4,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
 
 app.UseHttpsRedirection();
 
